34010f0f30b7ba1c0a638a2881eec5739f6c749b
jjppjjpp0099-ux <jjppjjpp0099@gmail.com>
2026-03-29 21:21:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS  # <--- Ye line 1 (Naya add karna hai)
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
 CORS(app)  # <--- Ye line 2 (Naya add karna hai)
 
 # Iske niche aapka purana sara code (tokens, protobuf, etc.) rahega
